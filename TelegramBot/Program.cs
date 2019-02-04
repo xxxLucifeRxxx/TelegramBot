8a5d1e6cc097299cd2d69f228e59fb9cf90d0371
Владислав Бандо <vlad.bando@mail.ru>
2019-02-04 10:13:09 +0800

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+	class Program
+	{
+		static TelegramBotClient bot;
+
+		static void Main(string[] args)
+		{
+			
+			bot = new TelegramBotClient("769411125:AAGASD2jpK-_vnG_VSu6RJ8yFMWofdBcCS0");
+
+			bot.OnMessage += Bot_OnMessageReceived;
+
+			var me = bot.GetMeAsync().Result;
+
+			Console.WriteLine(me.FirstName);
+			bot.StartReceiving();
+			Console.ReadLine();
+			bot.StopReceiving();
+		}
+
+		private static void Bot_OnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+		{
+			var message = e.Message;
+
+			Console.WriteLine(message.Text);
+		}
+	}
+}
