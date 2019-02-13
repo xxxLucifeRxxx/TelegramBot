2c71b4a0dbc33583928edf4ebf1a4aa1de71400f
Владислав Бандо <vlad.bando@mail.ru>
2019-02-14 00:25:45 +0800

    Добавляется начальная логика бота

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -6,23 +6,25 @@ using System.Net;
 using System.Text;
 using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
 
 namespace TelegramBot
 {
 	class Program
 	{
 		static TelegramBotClient bot;
-	    private string _key;
+		private string _key;
 
-     //   public TestBot(string key)
-	    //{
-	    //    _key = key;
+		//   public TestBot(string key)
+		//{
+		//    _key = key;
 
-	    //}
+		//}
 
-        static void Main(string[] args)
+		static void Main(string[] args)
 		{
-			
+			var states = new List<State>();
+
 			bot = new TelegramBotClient("769411125:AAGASD2jpK-_vnG_VSu6RJ8yFMWofdBcCS0");
 
 			bot.OnMessage += Bot_OnMessageReceived;
@@ -37,8 +39,8 @@ namespace TelegramBot
 
 		private static async void Bot_OnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
 		{
-		    StringBuilder msg = new StringBuilder();
-            var message = e.Message;
+			StringBuilder msg = new StringBuilder();
+			var message = e.Message;
 
 			if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
 			{
@@ -58,39 +60,114 @@ namespace TelegramBot
 
 					await bot.SendTextMessageAsync(message.Chat.Id, "Ну привет, удиви МЕНЯ!!!(/help)");
 
-					if(message.Text == "/help")
+					if (message.Text == "/help")
 					{
-						var keyboard = new InlineKeyboardMarkup(new Telegram.Bot.Types.InlineKeyboardButton[][]
-										{new [] {
-												 new Telegram.Bot.Types.InlineKeyboardButton("Текст для первой кнопки","callback1"),
-												 new Telegram.Bot.Types.InlineKeyboardButton("Текст второй кнопки","callback2"),
-												 },
-									   });
+						//var keyboard = new InlineKeyboardMarkup(new Telegram.Bot.Types.InlineKeyboardButton[][]
+						//				{new [] {
+						//						 new Telegram.Bot.Types.InlineKeyboardButton("Текст для первой кнопки","callback1"),
+						//						 new Telegram.Bot.Types.InlineKeyboardButton("Текст второй кнопки","callback2"),
+						//						 },
+						//			   });
 
-						bot.OnCallbackQuery += async (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
-						{
-							var messages = ev.CallbackQuery.Message;
-							if (ev.CallbackQuery.Data == "callback1")
-							{
-								// сюда то что нужно сделать при нажатии на первую кнопку 
-							}
-							else
-							if (ev.CallbackQuery.Data == "callback2")
-							{
-								// сюда то что сделать при нажатии на вторую кнопку
-							}
-						};
-					}
-
-
-
-
+						//bot.OnCallbackQuery += async (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
+						//{
+						//	var messages = ev.CallbackQuery.Message;
+						//	if (ev.CallbackQuery.Data == "callback1")
+						//	{
+						//		// сюда то что нужно сделать при нажатии на первую кнопку 
+						//	}
+						//	else
+						//	if (ev.CallbackQuery.Data == "callback2")
+						//	{
+						//		// сюда то что сделать при нажатии на вторую кнопку
+						//	}
+					};
 				}
 			}
+		}
+	}
 
-        }
+	public class MainState : IUpdateState
+	{
+		public void Update(object msg, object bot, int chatId, State state)
+		{
+			if (msg == "Привет")
+			{
+				//boi.Send(chatId, "Hi ,men, Как дела?")
+				state.StateChat = StateChat.Dialog;
+			}
+			else
+			{
+				//boi.Send(chatId, "Ввод неверной информации, повторитие")
+			}
+		}
+	}
+
+	public class Dialog : IUpdateState
+	{
+		public void Update(object msg, object bot, int chatId, State state)
+		{
+			if (msg == "У меня все хорошо!!!")
+			{
+				//boi.Send(chatId, "Это здорово, рад за тебя!!!")
+			}
+			else if (msg == "Домой")
+			{
+				state.StateChat = StateChat.Main;
+			}
+			else
+			{
+				//boi.Send(chatId, "Не понимсаю тебя, если хочешь на главную прсото скажи "Домой"")
+			}
+		}
+	}
 
 
+	public class Fsm
+	{
+		public Fsm(int chatId, List<State> states, object msg, object bot)
+		{
+			var state = states.FirstOrDefault(x => x.ChatId == chatId);
+			if (state == null)
+			{
+				state = new State() { ChatId = chatId, StateChat = StateChat.Main };
+				states.Add(state);
+			}
 
+			IUpdateState updateState;
+			switch (state.StateChat)
+			{
+				case StateChat.Main:
+					updateState = new MainState();
+					break;
+
+				case StateChat.Dialog:
+					updateState = new Dialog();
+					break;
+
+				default:
+					throw new AggregateException();
+			}
+			updateState.Update(msg, bot, chatId, state);
+		}
+	}
+
+	public class State
+	{
+		public int ChatId { get; set; }
+		public StateChat StateChat { get; set; }
+	}
+
+	internal interface IUpdateState
+	{
+		void Update(object msg, object bot, int chatId, State state);
+	}
+
+	public enum StateChat
+	{
+		Main,
+		Dialog
 	}
 }
+	
+
