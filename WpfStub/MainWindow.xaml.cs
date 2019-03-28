28d1e349dd33ceb8d3ae0a1e665a8c50c4360241
Владислав Бандо <vlad.bando@mail.ru>
2019-03-28 11:34:56 +0800

    Был доделан клиент приема заявок было реализовано все что планировалось

--- WpfStub/MainWindow.xaml.cs
+++ WpfStub/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using GMap.NET;
 
 namespace WpfStub
 {
@@ -23,7 +26,7 @@ namespace WpfStub
 					ListBoxMain.Items.Clear();
 					foreach (var item in GetData())
 					{
-						ListBoxMain.Items.Add("" + item);
+						ListBoxMain.Items.Add(item);
 					}
 				});
 			};
@@ -32,23 +35,68 @@ namespace WpfStub
 
 		}
 
-		private List<string> GetData()
+		private List<MyClass> GetData()
 		{
-			var list= new List<string>();
+			var list= new List<MyClass>();
 			var db = new db_botEntities();
 			var applications = db.Applications.Where(x => x.State == 1).ToList();
-		
+
 			foreach (var item in applications)
 			{
-				list.Add($"{item.NumbPhone} {item.Time} {item.PaymentMethod}");
+				list.Add(new MyClass()
+				{
+					Phone = item.NumbPhone,
+					Time = item.Time,
+					Payment = item.PaymentMethod,
+					LonFrom = item.LongitudeFrom,
+					LatFrom = item.LatitudeFrom,
+					LonTo = item.LongitudeTo,
+					LatTo = item.LatitudeTo
+				});
 			}
-			
+
+			var last = list.LastOrDefault();
+			if (last?.LatFrom != null && last.LonFrom.HasValue)
+				mapView.Position = new PointLatLng(last.LatFrom.Value, last.LonFrom.Value);
+
+			if (last?.LatTo != null && last.LonTo.HasValue)
+				mapView2.Position = new PointLatLng(last.LatTo.Value, last.LonTo.Value);
+
 			return list;
+		}
+
+		class MyClass
+		{
+			public string Phone { get; set; }
+			public DateTime? Time { get; set; }
+			public int? Payment { get; set; }
+
+			public string Pay => ((Payment ?? 0 ) == 1)? "Наличные":( ((Payment ?? 0) == 2)? "Мобильный банк":"");
+			public float? LonFrom { get; set; }
+			public float? LatFrom
+
+			{ get; set; }
+
+			public float? LatTo { get; set; }
+			public float? LonTo 
+			{ get; set; }
+
+			public override string ToString()
+			{
+				return $"{Phone:11} {Time?.ToShortTimeString()} {Pay}";
+			}
 		}
 
 
 		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+			var list = (ListBox) sender;
+			var myClass = (MyClass) list.SelectedValue;
+			if (myClass?.LatFrom != null && myClass.LonFrom.HasValue)
+				mapView.Position = new PointLatLng(myClass.LatFrom.Value, myClass.LonFrom.Value);
+
+			if (myClass?.LatTo != null && myClass.LonTo.HasValue)
+				mapView2.Position = new PointLatLng(myClass.LatTo.Value, myClass.LonTo.Value);
 
 		}
 
@@ -75,6 +123,41 @@ namespace WpfStub
 		private void MapView_OnLoaded(object sender, RoutedEventArgs e)
 		{
 			throw new System.NotImplementedException();
+		}
+
+		private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+			// choose your provider here
+			mapView.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+			mapView.MinZoom = 2;
+			mapView.MaxZoom = 18;
+			// whole world zoom
+			mapView.Zoom = 18;
+			// lets the map use the mousewheel to zoom
+			mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+			// lets the user drag the map
+			mapView.CanDragMap = true;
+			mapView.Position = new PointLatLng(51.828117, 107.588516);
+			// lets the user drag the map with the left mouse button
+			mapView.DragButton = MouseButton.Left;
+
+
+
+			GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+			// choose your provider here
+			mapView2.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+			mapView2.MinZoom = 2;
+			mapView2.MaxZoom = 18;
+			// whole world zoom
+			mapView2.Zoom = 18;
+			// lets the map use the mousewheel to zoom
+			mapView2.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+			// lets the user drag the map
+			mapView2.CanDragMap = true;
+			mapView2.Position = new PointLatLng(51.828117, 107.588516);
+			// lets the user drag the map with the left mouse button
+			mapView2.DragButton = MouseButton.Left;
 		}
 	}
 }
